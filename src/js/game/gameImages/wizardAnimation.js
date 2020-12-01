9ea24dfa57d94d2d7243243a7637b50aa44cc232
William Lennartsson <williamlennartsson@Williams-MacBook-Pro.local>
2020-12-01 12:23:40 +0100

    wizardAnimationFrames

---
+++ src/js/game/gameImages/wizardAnimation.js
@@ -0,0 +1,54 @@
+
+
+export const wizardAnimationFrames = {
+  idle: {
+    frames: [
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle1.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle2.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle3.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle4.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle5.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle6.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle7.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle8.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle8.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle10.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle11.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle12.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle13.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle14.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle15.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle16.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle17.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle18.png'),
+      require('./BlueWizard/2BlueWizardIdle/WizardIdle19.png')
+    ]
+  },
+  jump: {
+  },
+  walk: {
+      frames: [
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00001.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00002.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00003.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00004.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00005.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00006.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00007.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00008.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00009.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00010.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00011.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00012.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00013.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00014.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00015.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00016.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00017.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00018.png'),
+        require('./BlueWizard/2BlueWizardWalk/Chara_BlueWalk00019.png')
+      ]
+  }
+}
+
+     
