002e5ed546fd36f091e41af561feb13f163ffbd4
William Lennartsson <williamlennartsson@Williams-MacBook-Pro.local>
2020-12-03 16:49:17 +0100

    more assets

--- src/js/game/loaders.js
+++ src/js/game/loaders.js
@@ -1,10 +1,14 @@
 import character from './gameImages/Char.png'
 import backgroundTileset from './gameImages/overworldTileset/PNG/overworld.png'
 import samuraiWalkSpriteset from './gameImages/samurai/Boss_Samurai_Walk.png'
+import samuraiWalkData from './gameImages/samurai/Boss_Samurai_Walk.json'
+import samuraiDeathSpriteset from './gameImages/samurai/Boss_Samurai_Death.png'
+import samuraiDeathData from './gameImages/samurai/Boss_Samurai_Death.json'
 import level1 from './levels/level1.json'
 import sword from './gameImages/sword.png'
 
-import samuraiWalkData from './gameImages/samurai/Boss_Samurai_Walk.json'
+import plantImage1 from './gameImages/PlantAnimations/Plant1/Plant1_00000.png'
+
 
 export const loadLevel = (level) => {
   return level1
@@ -16,21 +20,29 @@ export const loadGameAssets = () => {
       loadCharacter(), // soon deprecated
       loadTileset(),
       loadSamuraiSpriteset(),
-      loadImage(sword)
+      loadImage(sword),
+      loadImage(samuraiDeathSpriteset),
+      loadImage(plantImage1)
     ]).then(([
       wizard,
       backgroundTileset,
-      samuraiSpriteset,
-      sword
+      samuraiWalkSpriteset,
+      sword,
+      samuraiDeathSpriteset,
+      plantImage
     ]) => {
       resolve({
         tilesets: {
           background: backgroundTileset, // fix so it looks like the others
-          player: {image: samuraiSpriteset, data: samuraiWalkData},
+          player: {
+            walk: {spriteSheet: samuraiWalkSpriteset, data: samuraiWalkData},
+            death: {spriteSheet: samuraiDeathSpriteset, data: samuraiDeathData }
+          },
           wizard: {sprite: wizard},
         },
         images: {
-            sword
+            sword,
+            plantImage
           }
       })
     })
