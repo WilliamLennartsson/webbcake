1dd0d293edac56ff17582ddfca7554218487ca57
William Lennartsson <williamlennartsson@Williams-MacBook-Pro.local>
2020-12-04 02:41:29 +0100

    idea about plantFactory

--- src/js/game/entities/entityFactory.js
+++ src/js/game/entities/entityFactory.js
@@ -1,15 +1,38 @@
 import Item from './item'
-import {plant1AnimationFrames} from '../gameImages/plantAnimations'
+import {plant1AnimationFrames, plant7AnimationFrames} from '../gameImages/plantAnimations'
+
+export const plantFactory = (assets, type) => {
+  
+  return (x, y) => {
+    //const frames = plant1AnimationFrames()
+    const frames = plant7AnimationFrames()
+    const plantW = 70
+    const plantH = 70
+    const plantConfig = {
+      sprite: assets.images.plantImage,
+      animated: true,
+      frames: frames,
+      animGroupName: 'Plant1',
+      animName: 'idle',
+      animSpeed: 4
+    }
+    const plant = new Item(plantConfig, x, y, plantW, plantH)
+    return plant
+  }
+}
 
 export const createPlant = (assets, x, y) => {
+  //const frames = plant1AnimationFrames()
+  const frames = plant7AnimationFrames()
   const plantW = 70
   const plantH = 70
   const plantConfig = {
     sprite: assets.images.plantImage,
     animated: true,
-    frames: plant1AnimationFrames(),
+    frames: frames,
     animGroupName: 'Plant1',
-    animName: 'idle'
+    animName: 'idle',
+    animSpeed: 4
   }
   const plant = new Item(plantConfig, x, y, plantW, plantH)
   return plant
