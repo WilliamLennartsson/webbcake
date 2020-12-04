f3e7a554d66d8eb0a0f0e0d5119de8131e18c2bf
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-12-04 15:17:07 +0100

    Created pyscript for generating spritesheet jsonfiles

---
+++ src/spritesheetGen/README.md
@@ -0,0 +1,8 @@
+# SpriteSheet Generator #
+How to use:
+
+python3 spritegenerator.py <width> <height> <framesPerRow> <framesPerColumn> filename
+
+Example:
+
+python3 spritegenerator.py 640 480 4 4 character.png
---
+++ src/spritesheetGen/spritegenerator.py
@@ -0,0 +1,80 @@
+import sys
+import json
+
+if len(sys.argv) > 1:
+
+    width = int(sys.argv[1])
+    height = int(sys.argv[2])
+    row = int(sys.argv[3])
+    col = int(sys.argv[4])
+    filename = sys.argv[5]
+    frameWidth = width / row
+    frameHeight = height / col
+    framesTot = row * col
+    
+    data = {}
+    frames =  {}
+    animations = {}
+    meta = {
+            'app': 'webbcake',
+            'image': filename,
+            'size': {
+                'w':'',
+                'h':''
+            }
+            }
+    x = 0
+    y = 0
+
+    for i in range(framesTot):
+
+        frames[filename + str(i)] = {
+            'frame': {
+                'x': int(x),
+                'y': int(y),
+                'w': 0,
+                'h': 0
+                },
+            'rotated': False,
+            'trimmed': False,
+            'spriteSourceSize': {
+                'w': int(frameWidth),
+                'h': int(frameHeight)
+                },
+            'sourceSize': {
+                'w': int(frameWidth),
+                'h': int(frameHeight)
+                }
+                }
+
+        if x + frameWidth == width:
+            x = 0
+            y += frameHeight
+
+        if y == height:
+            break
+        
+        x += frameWidth
+
+    data['frames'] = frames
+    data['meta'] = meta
+
+    with open(filename + '.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    print('Created ' + filename + '.json')
+
+else:
+    print('')
+    print('##########################')
+    print('# No arguments provided! #')
+    print('##########################')
+    print('')
+    print('Use:')
+    print('python3 spritegenerator.py <width> <height> <frames/row> <frames/column> <yourFilename>')
+    print('')
+    print('width: width of image')
+    print('height: height of images')
+    print('frames/row: spriteframes/row')
+    print('frames/col: spriteframes/col')
+    print('filename: Use filename of image')
